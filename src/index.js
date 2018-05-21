27f173c67c0dcf0ef4f32ee9052bedad3ef2b3ed
Beata Markunas <beata.markunas@gmail.com>
2018-05-21 22:33:16 +0200

    Add DevTools

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { getCountries } from './actions/actions-countries';
+import { createStore } from 'redux';
 
 
-ReactDOM.render(<h1>Inicjalizacja projektu</h1>, document.getElementById('root'));
+render(
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
+    document.getElementById('root')
+);
+
+store.dispatch(getCountries());
