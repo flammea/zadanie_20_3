22d001fc523bfe9c211be8593af37ee697887890
Beata Markunas <beata.markunas@gmail.com>
2018-05-23 19:04:18 +0200

    Add routing.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,25 @@ import { getCountries } from './actions/actions-countries';
 import { createStore } from 'redux';
 import DevTools from './DevTools';
 
+import { Router, Route, hashHistory } from 'react-router';
+import Navigation from './presentational/navigation.component';
+
+
+
 
 render(
     <Provider store={store}>
         <div>
-        	<h1>Inicjalizacja projektu</h1>
+        	<Router history={hashHistory}>
+            <Route path='/' component={Navigation}>
+
+            </Route>
+        	</Router>
         	<DevTools/>
         </div>
     </Provider>,
     document.getElementById('root')
 );
+
 
 store.dispatch(getCountries());
