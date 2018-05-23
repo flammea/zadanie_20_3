47ff22dc9210822349ed13b2679b7f8459d4a4e7
Beata Markunas <beata.markunas@gmail.com>
2018-05-23 19:46:12 +0200

    Create routes.js

--- src/index.js
+++ src/index.js
@@ -6,20 +6,15 @@ import { getCountries } from './actions/actions-countries';
 import { createStore } from 'redux';
 import DevTools from './DevTools';
 
-import { Router, Route, hashHistory } from 'react-router';
-import Navigation from './presentational/navigation.component';
-
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
 
 
 
 render(
     <Provider store={store}>
         <div>
-        	<Router history={hashHistory}>
-            <Route path='/' component={Navigation}>
-
-            </Route>
-        	</Router>
+        	<Router history={hashHistory} routes={routes}/>
         	<DevTools/>
         </div>
     </Provider>,
