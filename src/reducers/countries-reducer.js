9b7241ddcd5d3c75ed475dadc087e4fc377e9f1b
Beata Markunas <beata.markunas@gmail.com>
2018-05-24 22:37:50 +0200

    Create CountryDetails component.

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -10,6 +10,7 @@ const countriesReducer = function (state = initialState, action) {
     switch (action.type) {
         case GET_COUNTRIES:
             return Object.assign({}, state, {countries: state.countries});
+            
         case GET_COUNTRY:
 		  const selectedCountry= state.countries.find(country => country.id===parseInt(action.id));
 		  return Object.assign({}, state, {selectedCountry});
